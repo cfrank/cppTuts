5fe4e489849c73e16d66a82590d1b0faad840846
Chris Frank <chris@cfrank.org>
2013-03-27 22:50:51 -0400

    Initial Commit
    Simple c++ program to remove all vowels in a string provided from the
    user.

---
+++ cppOMO.cpp
@@ -0,0 +1,68 @@
+// cppOMO.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <math.h>
+
+std::string input;
+int i, ilen;
+double count1, count2;
+
+double percent(double start, double end);
+
+int main()
+{
+	double perc1;
+
+	std::cout<<"Welcome to the disemvoweling utility!"<<std::endl;
+	std::cout<<"Enter your phrase: ";
+	std::getline(std::cin, input);
+
+	count1 = input.length();
+	std::cout<<"The disemvoweled phrase is: ";
+
+	for(std::size_t i = 0, ilen = input.length(); i < ilen; ++i)
+	{
+		switch(input[i])
+		{
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+			input[i] = '\n';
+		}
+		if(input[i] != '\n')
+		{
+			// Print each letter.
+			std::cout<<input[i];
+		}
+		else
+		{
+			std::cout<<"";
+			// Subtract from end count.
+			--count2;
+		}
+		++count2;
+	}
+	perc1 = percent(count1, count2);
+
+	std::cout<<"\nStart "<< count1 <<" End "<< count2 <<std::endl;
+	std::printf("Reduction Rate of: %4.1f%% \n", perc1 * 100);
+
+	// Keep cmdP active until close.
+	std::cin.get();
+	return 0;
+}
+
+double percent(double start, double end)
+{
+	double perc, change;
+
+	change = start - end;
+	perc = (int)change / start;
+	return perc;
+}
+
